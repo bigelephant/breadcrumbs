a085d7bb7a00d257ce9bfbb2a1ad634a2dcce2ad
Robbo- <robert@mercenarydesign.net>
2012-12-09 14:38:35 +1100

    - very basic functionality done

--- src/BigElephant/Breadcrumbs/Breadcrumbs.php
+++ src/BigElephant/Breadcrumbs/Breadcrumbs.php
@@ -21,6 +21,38 @@ class Breadcrumbs implements \Countable, \IteratorAggregate {
 	}
 
 	/**
+	 * Get a new breadcrumbs instance.
+	 *
+	 * @param  array  $items
+	 */
+	public function make(array $items)
+	{
+		return new self($items);
+	}
+
+	/**
+	 * Add a crumb, is chainable.
+	 *
+	 * @param string 	title
+	 * @param string 	href
+	 * @param array 	optons
+	 *
+	 * @return BigElephant\Breadcrumbs\Breadcrumbs
+	 */
+	public function add($title, $href, array $options = array())
+	{
+		$this->crumbs[count($this->crumbs) - 1]['active'] = false;
+
+		$this->crumbs[] = array(
+			'title' => $title,
+			'href' => $href,
+			'active' => true
+		);
+
+		return $this;
+	}
+
+	/**
 	 * Get an iterator for the crumbs.
 	 *
 	 * @return ArrayIterator
@@ -39,6 +71,4 @@ class Breadcrumbs implements \Countable, \IteratorAggregate {
 	{
 		return count($this->crumbs);
 	}
-
-	public function test() { echo 'test'; }
 }
